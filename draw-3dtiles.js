3bf4a215defcc295fb63b93f890af8cf95b31677
laurenwalker <walker@nceas.ucsb.edu>
2022-04-14 11:32:30 -0400

    Color 3D tiles randomly, to better show off the geohash shapes

--- draw-3dtiles.js
+++ draw-3dtiles.js
@@ -18,41 +18,82 @@ const viewer = new Cesium.Viewer('cesiumContainer', {
 }
 });
 
+window.tileset;
+var counts = {};
+
 drawGeohashes = function(){
-  var tileset = new Cesium.Cesium3DTileset({
+  tileset = new Cesium.Cesium3DTileset({
     url: "tilesets/geohash/tileset.json",
+    debugWireframe: false,
     debugShowBoundingVolume: false,
     debugShowContentBoundingVolume: false
   });
 
   tileset.style = new Cesium.Cesium3DTileStyle({
-    color: {
-      conditions: [
-        ["true", "color('DARKTURQUOISE', 0.5)"],
-      ],
-    },
     show: true,
   });
   viewer.scene.primitives.add(tileset);
 
-
-  bboxes(-90,-180,90,180,2).forEach( (hash) => {
-
-    let bbox = decode_bbox(hash),
-        count = parseInt(Math.random()*100);
-
-    let label = viewer.entities.add({
-      position : Cesium.Cartesian3.fromDegrees((bbox[2]+bbox[0])/2, (bbox[3]+bbox[1])/2),
-      label : {
-          text : count.toString(),
-          font : '14pt monospace',
-          style: Cesium.LabelStyle.FILL,
-          outlineWidth : 0,
-          //Make label text smaller when zoomed out, larger when zoomed in
-          scaleByDistance : new Cesium.NearFarScalar(1.5e2, 5.0, 8.0e6, 0.7)
-      }
-    });
+  tileset.allTilesLoaded.addEventListener(() => {
+    drawLabels()
+    changeColors()
   });
+
+  drawLabels = function(){
+
+    bboxes(-90,-180,90,180,2).forEach( (hash) => {
+
+      let bbox = decode_bbox(hash),
+          count = parseInt(Math.random()*100);
+
+      //Save the count for this hash in the count map
+      counts[hash] = count;
+
+      let label = viewer.entities.add({
+        position : Cesium.Cartesian3.fromDegrees((bbox[3]+bbox[1])/2, (bbox[2]+bbox[0])/2),
+        label : {
+            text : hash,//count.toString(),
+            font : '14pt monospace',
+            style: Cesium.LabelStyle.FILL,
+            outlineWidth : 0,
+            //Make label text smaller when zoomed out, larger when zoomed in
+            scaleByDistance : new Cesium.NearFarScalar(1.5e2, 5.0, 8.0e6, 0.7)
+        }
+      });
+
+        //Draw polygons to "outline" the 3D Tiles.
+        //Commented out because it's too slow to work well.
+      /*polygon = viewer.entities.add({
+        polygon : {
+          hierarchy : Cesium.Cartesian3.fromDegreesArray([ 
+            bbox[1], bbox[0],
+            bbox[3], bbox[0],  
+            bbox[3], bbox[2],
+            bbox[1], bbox[2] ]),
+          height : 1,
+          material : Cesium.Color.fromHsl(170/360, .5, .5, 0),
+          outline : true,
+          outlineColor : Cesium.Color.WHITE
+        }
+      });*/
+      
+    });
+    
+  }
+
+  changeColors = function(){
+    tileset.style = new Cesium.Cesium3DTileStyle({
+      color : { 
+        evaluateColor : function(feature, result) { 
+          console.log(feature.getProperty("id"))
+          let alpha = (["yt", "28", 28].indexOf(feature.getProperty("geohash"))>-1)? 0 : 1;
+          let percent = counts[feature.getProperty("geohash")]/100;
+          return Cesium.Color.fromHsl(Math.floor(Math.random() * 360)/360, 0.5, 0.6, alpha);
+    }}})
+  }
+ 
+
+
 } 
 
 const t0 = performance.now();
